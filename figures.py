d7718e798e8cfd3b394e1938b955d54fd5b743ea
Stanislaw Adaszewski <sadaszewski@Stanislaws-MacBook-Air.local>
2015-09-03 16:31:00 +0200

    Added figure renumbering support.

--- figures.py
+++ figures.py
@@ -9,6 +9,7 @@
 
 from markdown import Extension
 from markdown.blockprocessors import BlockProcessor
+from markdown.treeprocessors import Treeprocessor
 from markdown.util import etree, AtomicString
 import numpy as np
 from collections import defaultdict
@@ -22,6 +23,7 @@ class FigureExtension(Extension):
         md.parser.blockprocessors.add('figure',
                                       FigureProcessor(md.parser),
                                       '<hashheader')
+        md.treeprocessors.add('figure', FigureTreeProcessor(md), '<prettify')
 
 
 def makeExtension(configs={}):
@@ -32,7 +34,7 @@ class FigRefPattern(Pattern):
     def handleMatch(self, m):
         hash = m.group(2).lower().replace(' ', '_')
         a = etree.Element('a')
-        a.set('href', '#%s' % hash)
+        a.set('href', '#figref_%s' % hash)
         a.text = AtomicString('[%s]' % m.group(2))
         return a
 
@@ -48,5 +50,46 @@ class FigureProcessor(BlockProcessor):
         p = etree.SubElement(parent, 'p')
         a = etree.SubElement(p, 'a')
         hash = m.group(0).lower().replace('.','').replace(' ', '_')
-        a.set('name', '%s' % hash)
+        a.set('name', 'figref_%s' % hash)
         a.text = '%s %s' % (m.group(0), caption)
+
+
+class FigureTreeProcessor(Treeprocessor):
+    def run(self, root):
+        # print 'Running...', dir(self)
+        cnt = defaultdict(lambda: 0)
+        M = {}
+
+        Q = [root]
+        while len(Q) > 0:
+            el = Q.pop(0)
+            for ch in el: Q.append(ch)
+            name = el.get('name')
+            if el.tag == 'a' and name is not None and name.startswith('figref_'):
+                type_ = name.split('_')[1]
+                title = '.'.join(el.text.split('.')[1:])
+                if name not in M:
+                    cnt[type_] += 1
+                    M[name] = str(cnt[type_])
+
+                el.text = type_[0].upper() + type_[1:] + ' ' + M[name] + '. ' + title
+
+        Q = [root]
+        while len(Q) > 0:
+            el = Q.pop(0)
+            # print 'Here', el
+            for ch in el:
+                Q.append(ch)
+
+            href = el.get('href')
+            if el.tag == 'a' and href is not None and href.startswith('#figref_'):
+                type_ = href.split('_')[1]
+                #if href in M:
+                #    pass
+                #else:
+                #    cnt[type_] += 1
+                #    M[href] = str(cnt[type_])
+
+                el.text = '[%s %s]' % (type_[0].upper() + type_[1:], M[href[1:]])
+
+
